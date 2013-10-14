8edcffd0ead6accc4a5da92845e5747ca116be16
Joe Montibello <josephmontibello@gmail.com>
2013-10-14 15:41:22 -0400

    Initial commit of files.

---
+++ README.md
@@ -0,0 +1,3 @@
+Python library for making ncip requests to a Millennium ILS server
+with milnciprelais on it.
+
---
+++ ncip.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+
+import sys
+sys.path.append('/Users/dltg/ncip')
+import ncipcfg
+
+x=NcipRequest()
+
+print x.PatronBarcode
