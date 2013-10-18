1866da75db230acb5e0b3c8ea4a9c2c095ab2b62
joemontibello <josephmontibello@gmail.com>
2013-10-18 15:27:54 -0400

    Working towards handling command line arguments

--- ncip.py
+++ ncip.py
@@ -1,9 +1,35 @@
 #!/usr/bin/python
 
 import sys
-sys.path.append('/Users/dltg/ncip')
+sys.path.append('./')
 import ncipcfg
+import getopt
 
+#testing only
 x=ncipcfg.NcipRequest()
+#print x.PatronBarcode
 
-print x.PatronBarcode
+def usage():
+    print "this is a usage statement"
+
+#accept arguments
+def main(argv):                                       
+    try:                                
+        #opts, args = getopt.getopt(argv, "hg:d", ["help", "grammar="])
+        opts, args = getopt.getopt(argv, "hu:i:p:r:n:x:", ["help", "ResponderURL=", "ItemBarcode=", "PatronBarcode=", "RequestType=", "NCIPVersion=", "XMLNS="])
+    except getopt.GetoptError:           
+        usage()                          
+        sys.exit(2)                     
+        if __name__ == "__main__":
+            #print "name=main"
+            main(sys.argv[1:])
+            for opt, arg in opts:
+                if opt in ("-h", "--help"):      
+                    usage()                     
+                    sys.exit()
+                elif opt == '-d':                
+                    global _debug               
+                    _debug = 1
+                else:
+                    usage()
+
--- ncipcfg.py
+++ ncipcfg.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 
+
+
 class NcipRequest:
     """An NCIP request in object form"""
-    ResponderURL = "http://129.170.20.40/iii/nciprelais/Restful"
-    ItemBarcode= "33312001847777"
-    PatronBarcode="23311001905938"
-    RequestType="LookupUser"
-    NCIPVersion="http://www.niso.org/schemas/ncip/v2_0/ncip_v2_0.xsd"
-    XMLNS="http://www.niso.org/2008/ncip"
-
+    ResponderURL = "http://129.170.20.40/iii/nciprelais/Restful" # -u
+    ItemBarcode= "33312001847777" # -i
+    PatronBarcode="23311001905938" # -p
+    RequestType="LookupUser" # -r
+    NCIPVersion="http://www.niso.org/schemas/ncip/v2_0/ncip_v2_0.xsd" # -n
+    XMLNS="http://www.niso.org/2008/ncip" # -x
+    
